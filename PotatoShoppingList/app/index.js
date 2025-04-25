5243730b8701a8c7ebbcf7a8226f3c2552989cd7
varraschin <marcelojuniber@gmail.com>
2025-04-25 12:28:53 -0300

    Pagina Inicial

--- PotatoShoppingList/app/index.js
+++ PotatoShoppingList/app/index.js
@@ -1,11 +1,29 @@
+import { Link } from 'expo-router'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function Welcome() {
   return (
     <View style={styles.container}>
-      <Text>Olá</Text>
-      <StatusBar style="auto" />
+      <View style={styles.container}>
+        <Image source={require('../assets/avatar.png')} 
+        style={styles.avatar}
+        />
+      </View>
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Nerv Shopping List
+        </Text>
+        <Text style={styles.text}>
+          Monte sua lista de compras e não esqueça mais 
+          o que precisa comprar ao sair de casa!!!
+        </Text>
+        <Link style={StyleSheet.button} href={"/home"}>
+        <Text style={styles.buttonText}>Acessar</Text>
+        </Link>
+      </View>
+
+      <StatusBar style="light" backgroundColor='#000' />
     </View>
   );
 }
@@ -13,8 +31,51 @@ export default function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
+  },
+  containerImage: {
+    flex: 2,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  avatar: {
+    width: 230,
+    height: 230,
+    borderRadius: 115,
+    resizeMode: 'cover',
+  },
+  content: {
+    flex: 1,
+    backgroundColor: '#dadada',
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingHorizontal: '5%'
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: 'red',
+    marginTop: 28,
+    marginBottom: 20,
+    textAlign: 'center'
+  },
+  text: {
+    fontSize: 16,
+    color: 'gray'
+  },
+  button: {
+    position: 'absolute',
+    bottom: '12%',
+    backgroundColor: 'red',
+    alignSelf: 'center',
+    borderRadius: 50,
+    paddingVertical: 15,
+    width: '55%',
+    textAlign: 'center'
+  },
+  buttonText: {
+    fontSize: 22,
+    color: '#fff',
+    fontWeight: 'bold'
+  }
 });
