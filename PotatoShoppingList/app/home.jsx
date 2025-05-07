fb5f3eb6e8e85a3a4d65f9385755c1cd27545b26
varraschin <marcelojuniber@gmail.com>
2025-05-07 13:57:16 -0300

    Começando a Segunda Página

--- PotatoShoppingList/app/home.jsx
+++ PotatoShoppingList/app/home.jsx
@@ -1,12 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Home() {
   return (
-    <View>
-      <Text>H</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1}}>
+      <ImageBackground source={require('../assets/background.jpg')} style={{flex: 1, justifyContent: 'flex-start'}} resizeMode='repeat'>
+        <View style={styles.header}>
+          <Text style={styles.title}>Lista de Produtos</Text>
+          <Ionicons name="trash" size={32} color="#fff" />
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  header: {
+    padding: 25,
+    paddgingTop: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#000000c0',
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+  },
+  title: {
+    color: "#fff",
+    fontSize: 26,
+    fontWeight: 'bold'
+  }
+})
