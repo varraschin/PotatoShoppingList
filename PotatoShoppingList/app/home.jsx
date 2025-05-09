6ca332a1cc2a2d0d2e2a88626ed6a47e46bfb89e
varraschin <marcelojuniber@gmail.com>
2025-05-09 12:25:19 -0300

    Finalizando a Interface

--- PotatoShoppingList/app/home.jsx
+++ PotatoShoppingList/app/home.jsx
@@ -1,4 +1,4 @@
-import { ImageBackground, StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Ionicons } from '@expo/vector-icons';
@@ -11,6 +11,18 @@ export default function Home() {
           <Text style={styles.title}>Lista de Produtos</Text>
           <Ionicons name="trash" size={32} color="#fff" />
         </View>
+
+        <View style={{ flex: 1 }}></View>
+
+        <View style={styles.footer}>
+          <View style={styles.inputContainer}>
+          <TextInput color="#fff" fontSize={18}/>  
+          </View>
+          <TouchableOpacity style={styles.iconContainer}>
+            <Ionicons name='add' size={36} color='#fff' />
+          </TouchableOpacity>
+        </View>
+
       </ImageBackground>
     </SafeAreaView>
   )
@@ -31,5 +43,36 @@ const styles = StyleSheet.create({
     color: "#fff",
     fontSize: 26,
     fontWeight: 'bold'
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: '#000000c0',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30
+  },
+  inputContainer: {
+    backgroundColor: '#000',
+    elevation: 40,
+    flex: 1,
+    height: 50,
+    marginVertical: 17,
+    borderRadius: 30,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+
+  },
+  iconContainer : {
+    height: 50,
+    width: 50,
+    backgroundColor: '#000',
+    borderRadius: 25,
+    elevation: 40,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 })
